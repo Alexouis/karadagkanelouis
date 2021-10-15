ca85aa9bd32f74756d96453d0692771a5d484ece
Nouhou <soujilo54leymane@gmail.com>
2021-10-15 11:51:36 +0200

    conflict solved

--- src/client/main.cpp
+++ src/client/main.cpp
@@ -1,26 +1,4 @@
 #include <iostream>
-#include <fstream>
-#include <string.h>
-#include <sstream>
-#include <map>
-#include <memory>
-#include <unistd.h>
-#include <thread>
-//#include <microhttpd.h>
-#include <sys/socket.h>
-#include <netinet/in.h>
-#include <sys/types.h>
-
-#include <SFML/Graphics.hpp>
-#include <SFML/Audio.hpp>
-#include <SFML/Network.hpp>
-
-#include <state.h>
-#include "client.h"
-
-
-
-#include <iostream>
 
 // Les lignes suivantes ne servent qu'à vérifier que la compilation avec SFML fonctionne
 #include <SFML/Graphics.hpp>
@@ -43,19 +21,10 @@ int main(int argc,char* argv[])
 
     cout << "It works !" << endl;
 
-<<<<<<< HEAD
-    state::Player p{};
-    
-    //const string name = "Erza";
-    //Position position{1,2};
-    //Player player{};
-    //player.setName("Alex"); 
-=======
     Position pos;
     pos.setX(12);
 
     cout << pos.getX() << endl;
->>>>>>> 6e0998b1c3e460cec29a6dd6c7c1b4650f8ef02a
 
     return 0;
 }
