2f39ddf079d6290544bf4279b6ac6c068fd2f632
Nouhou <soujilo54leymane@gmail.com>
2021-11-09 16:27:54 +0100

    tmx loader done :)

--- src/client/main.cpp
+++ src/client/main.cpp
@@ -5,6 +5,15 @@
 
 void testSFML() {
     sf::Texture texture;
+    if (!texture.loadFromFile("../../res/synthetic grass/ground_grass_gen_07.png"))
+    {
+        // error...
+    }
+
+    sf::Sprite sprite;
+    sprite.setTexture(texture);
+    sf::RenderWindow window(sf::VideoMode(200, 200), "SFML works!");
+    window.draw(sprite);
 }
 
 // Fin test SFML
@@ -18,25 +27,30 @@ int main(int argc,char* argv[])
 {
     Exemple exemple;
     exemple.setX(53);
-    testSFML();
-    sf::RenderWindow window(sf::VideoMode(200, 200), "SFML works!");
+    //testSFML();
+    sf::Texture texture;
+    if (!texture.loadFromFile("/home/mkas/ENSEA/karadagkanelouis/res/grass/grass5.png")){
+        // error...
+        cout << "no texture! " << endl; 
+    }
+
+    sf::Sprite sprite;
+    sprite.setTexture(texture);
+    sf::RenderWindow window(sf::VideoMode(800, 800), "SFML works!");
     sf::CircleShape shape(100.f);
     shape.setFillColor(sf::Color::Green);
 
     while (window.isOpen())
     {
-        sf::Event event;
-        while (window.pollEvent(event))
-        {
-            if (event.type == sf::Event::Closed)
-                window.close();
-        }
+        sf::Event event;      while (window.pollEvent(event))      {          if (event.type == sf::Event::Closed)              window.close();      }
 
         window.clear();
-        window.draw(shape);
+        //window.draw(shape);
+        window.draw(sprite);
         window.display();
     }
-
+//
+    //MapLoader::MapLoader("res/map");
     cout << "It works !" << endl;
 
 }
