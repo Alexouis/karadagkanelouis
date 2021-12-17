81477b572e0ba1008c9ce5140b0f6f618a098105
karadag <kmusti95@hotmail.com>
2021-12-17 16:32:48 +0100

    fight/window

--- src/client/render/FightScene.cpp
+++ src/client/render/FightScene.cpp
@@ -15,13 +15,6 @@ namespace render{
         }
     };
 
-    const state::State*& FightScene::getGState() const{
-       // return gState;
-    };
-
-    void FightScene::setGState(const state::State*& gState){
-       // this->gState=&gState;
-    };
 
     void FightScene::draw (sf::RenderTarget& target, sf::RenderStates states) const{
 
