e4036e9d381554af97f5df5c3c3096c0cfab8a35
Nouhou <soujilo54leymane@gmail.com>
2021-12-29 00:30:39 +0100

    fixing Action bug

--- src/shared/engine/Engine.cpp
+++ src/shared/engine/Engine.cpp
@@ -11,8 +11,9 @@
 #include <iostream>
 #include "engine.h"
 
-#define CODE_MASk(X) ((X & 0xF0) >> 4)
-#define VALUE_MASK(X) (X & 0xF)
+#define CODE_MASk(X)  ((X & 0xF0) >> 4)
+#define VALUE_MASK(X) ( X & 0xF )
+#define IS_MOVE(X)    ((X & 0xF0) >> 7)
 
 #define SPELL1 0x0 //[code value] =  [0000 0000]
 #define SPELL2 0x1 //[code value] =  [0000 0001]
@@ -71,9 +72,13 @@ namespace engine{
         this->currentState = gState;
     }
 
-    void Engine::registerTarget (int x, int y){
-        this->cmdHolder = std::unique_ptr<Command>(new Command(this->action[CODE_MASk(this->selected)], x, y));
-        this->qcmd.push(std::move(cmdHolder));
+    void Engine::registerTarget (int x, int y, char selected){
+        if(IS_MOVE(selected)){
+            this->cmdHolder = std::unique_ptr<Command>(new Command(this->action[CODE_MASk(this->selected)], x, y));
+            this->qcmd.push(std::move(cmdHolder));
+            this->selected = selected;
+        }
+        
     }
 
     void Engine::registerTarget (char selected){
