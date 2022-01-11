5d330d14e170afc8bdf29a3d9a6b61b641cd3616
spide <spidersuper2@live.fr>
2022-01-11 10:32:29 +0100

    Ajout commentaires GameWindow

--- src/client/render/GameWindow.cpp
+++ src/client/render/GameWindow.cpp
@@ -26,11 +26,7 @@ namespace render {
 
     };
 
-    void GameWindow::init ()
-    {
-        initScenes();
-    };
-
+    //  Crée et initialise les 3 scènes requises pour le menu, le combat et la fin de partie.
     void GameWindow::initScenes ()
     {   
         std::unique_ptr<Scene> holder = std::unique_ptr<Scene>(new Scene(0,"start",this)); 
@@ -41,12 +37,13 @@ namespace render {
         this->scenes.push_back(std::move(holder));
     };
 
+    //  Affiche la fenêtre avec les paramètres désirés ainsi que la scène actuelle.
     void GameWindow::draw()
     {   
         this->window.draw(*(this->scenes[this->currentScene]));
     };
 
-    //void GameWindow::update()
+    //  Actualise le contenu de la fenêtre.
     void GameWindow::update (){
         this->scenes[this->currentScene]->update();
         if(this->currentScene == FIGHTSCENE && this->scenes[this->currentScene]->isGameOver())
@@ -55,17 +52,76 @@ namespace render {
         }
     };
     
+    /*  Actualise le contenu de la fenêtre et transmet des informations telles que la position du clic ou 
+        le type d’évènement.*/
     void GameWindow::update (sf::Event& e, sf::Vector2i m_mousePosition)
     {
         this->scenes[this->currentScene]->update(e, m_mousePosition,this);
         this->isZoomed = 0;
     };
 
-    
+    //  pour effacer le contenu de la fenêtre.
     void GameWindow::clearBackground()
     {
         this->bgTex.clear();
     };
+
+    //  permet de partager le state avec l’engine.
+    void GameWindow::shareStateWith(engine::Engine& ngine){
+        (this->scenes[SceneId::FIGHTSCENE])->bindState(ngine);
+    }
+
+    /*  permet de passer des coordonnées orthogonales en pixel 
+        de la fenêtre aux coordonnées de la map (cases) du jeu. */
+    sf::Vector2f GameWindow::screenToWorld (sf::Vector2f position){
+        return (this->scenes[SceneId::FIGHTSCENE])->screenToWorld(position);
+    }
+    
+    //  fait l’opération inverse.
+    sf::Vector2f GameWindow::worldToScreen (sf::Vector2f position){
+        return (this->scenes[SceneId::FIGHTSCENE])->worldToScreen(state::Position((int)(position.x), (int)(position.y)));
+    }
+
+    /*  écoute les actions du joueur (clic, appui sur le clavier, mouvement de souris, etc..) 
+        et conduit au déroulement adéquat en appelant les fonctions appropriées.    */
+    void GameWindow::handleEvents (sf::Event& event, sf::Vector2f& mousePosScreen, sf::Vector2f& mousePosWorld, engine::Engine& ngine){
+        if(event.type == sf::Event::MouseButtonPressed && event.mouseButton.button == sf::Mouse::Left){
+            this->selected = (char)MOVE;
+            this->update(event,(sf::Vector2i)mousePosScreen);
+            ngine.registerTarget(floor(mousePosWorld.x), floor(mousePosWorld.y), this->selected );
+        }
+        if(event.type == sf::Event::MouseMoved )
+        {
+            this->update(event,(sf::Vector2i)mousePosScreen);
+        }
+    }
+
+    /*  Fonctionne de manière similaire à handleEvents, cependant cette fonction n’écoute que la molette de la 
+        souris, associée à l’ action de zoom/dézoom.    */
+    void GameWindow::handleZoom (sf::Event& event, sf::Vector2f& mousePosScreen){
+        if (event.type == sf::Event::KeyPressed && event.key.code == sf::Keyboard::D)
+        {
+            this->update(event,(sf::Vector2i)mousePosScreen);
+        }
+        if(event.type == sf::Event::MouseButtonPressed && event.mouseButton.button == sf::Mouse::Right)
+        {
+            this->setCenter(mousePosScreen);
+
+            this->update(event,(sf::Vector2i)mousePosScreen);
+        }
+        if(event.type == sf::Event::MouseWheelMoved && this->getCurrentScene() == SceneId::FIGHTSCENE)
+        {
+            if(event.mouseWheel.delta == 1){
+                this->setZoom(0.8); 
+                this->isZoomed = 1;  
+            }
+            else{
+                this->setZoom(1.25); 
+                this->isZoomed = 1;    
+            }   
+            this->update(event,(sf::Vector2i)mousePosScreen);
+        }
+    }
 
     // Setters and Getters
 
@@ -162,61 +218,6 @@ namespace render {
         this->view.setCenter(center.x,center.y);
         this->window.setView(this->view); 
     };
-
-
-    void GameWindow::shareStateWith(engine::Engine& ngine){
-        (this->scenes[SceneId::FIGHTSCENE])->bindState(ngine);
-    }
-
-    
-    sf::Vector2f GameWindow::screenToWorld (sf::Vector2f position){
-        return (this->scenes[SceneId::FIGHTSCENE])->screenToWorld(position);
-    }
-    
-
-    sf::Vector2f GameWindow::worldToScreen (sf::Vector2f position){
-        return (this->scenes[SceneId::FIGHTSCENE])->worldToScreen(state::Position((int)(position.x), (int)(position.y)));
-    }
-
-
-    void GameWindow::handleEvents (sf::Event& event, sf::Vector2f& mousePosScreen, sf::Vector2f& mousePosWorld, engine::Engine& ngine){
-        if(event.type == sf::Event::MouseButtonPressed && event.mouseButton.button == sf::Mouse::Left){
-            this->selected = (char)MOVE;
-            this->update(event,(sf::Vector2i)mousePosScreen);
-            ngine.registerTarget(floor(mousePosWorld.x), floor(mousePosWorld.y), this->selected );
-        }
-        if(event.type == sf::Event::MouseMoved )
-        {
-            this->update(event,(sf::Vector2i)mousePosScreen);
-        }
-    }
-
-
-    void GameWindow::handleZoom (sf::Event& event, sf::Vector2f& mousePosScreen){
-        if (event.type == sf::Event::KeyPressed && event.key.code == sf::Keyboard::D)
-        {
-            this->update(event,(sf::Vector2i)mousePosScreen);
-        }
-        if(event.type == sf::Event::MouseButtonPressed && event.mouseButton.button == sf::Mouse::Right)
-        {
-            this->setCenter(mousePosScreen);
-
-            this->update(event,(sf::Vector2i)mousePosScreen);
-        }
-        if(event.type == sf::Event::MouseWheelMoved && this->getCurrentScene() == SceneId::FIGHTSCENE)
-        {
-            if(event.mouseWheel.delta == 1){
-                this->setZoom(0.8); 
-                this->isZoomed = 1;  
-            }
-            else{
-                this->setZoom(1.25); 
-                this->isZoomed = 1;    
-            }   
-            this->update(event,(sf::Vector2i)mousePosScreen);
-        }
-    }
-    
 
     GameWindow::~GameWindow ()
     {
