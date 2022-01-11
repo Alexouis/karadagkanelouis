cd27332a6f19e7028f16ab09571dc2df4c0c0838
spide <spidersuper2@live.fr>
2022-01-11 10:44:05 +0100

    Ajout commentaires Scene

--- src/client/render/Scene.cpp
+++ src/client/render/Scene.cpp
@@ -22,15 +22,17 @@ namespace render {
     };
 
     
-
+    //  Pour charger les images qui vont être affichées.
     void Scene::loadImages (){
 
     };
 
+    //  Pour charger les texture des éléments à afficher.
     void Scene::loadTextures (std::string textures_paths){
         this->texture.loadFromFile(textures_paths); // load texture de personnage si scene de combat
     };
 
+    //  Gère l'initialisation de la scène.
     void Scene::init (std::string type, GameWindow* gameWindow){
         sf::Vector2f size;
         sf::Vector2i pos;
@@ -69,15 +71,19 @@ namespace render {
         
     };
 
+    //  Permet d’actualiser le contenu de la fenêtre.
     void Scene::update(){
     };
     
+    /*  Permet d’actualiser le contenu de la fenêtre et de transmettre des informations tel que 
+        la position du clic ou le type d’évènement. */
     void Scene::update(sf::Event& e, sf::Vector2i m_mousePosition, GameWindow* gameWindow){        
         for(const auto& [key, value] : this->boxes){
             (*value).update(e,m_mousePosition, gameWindow);
         }
     };
 
+    //  Est utilisée afin de définir le background d’une scène.
     void Scene::setTexture(std::string stexture)
     {
         if (!(this->texture).loadFromFile(stexture))
@@ -90,7 +96,44 @@ namespace render {
         }
     }
 
-    // Setters and Getters
+    // permet de connaître l’état du jeu, s’il y a Game Over, on pourra mettre à jour la variable gameOver.
+    bool Scene::isGameOver()
+    {
+          return this->gameOver;
+    }
+
+    //  permet de connaître la classe du vainqueur. Cette fonction est appelée si isGameOver() renvoie true.
+    state::playerClass Scene::whosWinner()
+    {
+          return this->winner;
+    }
+
+    //  Pour dessiner et afficher à l’écran les éléments de la scène.
+    void Scene::draw (sf::RenderTarget& target, sf::RenderStates states) const{
+        target.draw(this->sprite, states);
+        for (const auto& [key, value] : boxes){
+            target.draw(*value,states);
+        }
+    }
+
+    //  Pour partager le state avec les scènes.
+    void Scene::bindState (engine::Engine& ngine){
+
+    }
+
+    /*  Permet de passer des coordonnées orthogonales en pixel de la fenêtre aux coordonnées de la 
+        map (cases) du jeu. */
+    sf::Vector2f Scene::screenToWorld (sf::Vector2f position){
+
+    }
+
+    //  Fait l’opération inverse.
+    sf::Vector2f Scene::worldToScreen (state::Position position){
+
+    }
+
+    //-----------------------------Setters and Getters-----------------------------
+
     const std::map<std::string,std::unique_ptr<Box>>& Scene::getBoxes() const{
         return this->boxes;
     };
@@ -105,36 +148,6 @@ namespace render {
     void Scene::setId(char id){
         this->id = id;
     };
-
-    bool Scene::isGameOver()
-    {
-          return this->gameOver;
-    }
-
-    state::playerClass Scene::whosWinner()
-    {
-          return this->winner;
-    }
-
-
-    void Scene::draw (sf::RenderTarget& target, sf::RenderStates states) const{
-        target.draw(this->sprite, states);
-        for (const auto& [key, value] : boxes){
-            target.draw(*value,states);
-        }
-    }
-
-    void Scene::bindState (engine::Engine& ngine){
-
-    }
-
-    sf::Vector2f Scene::screenToWorld (sf::Vector2f position){
-
-    }
-
-    sf::Vector2f Scene::worldToScreen (state::Position position){
-
-    }
 
     Scene::~Scene(){
 
