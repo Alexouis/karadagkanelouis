19054941339007f2e7d59efadf077f201c3654d2
Nouhou <soujilo54leymane@gmail.com>
2022-01-16 17:39:45 +0100

    deep ai : stratehy making

---
+++ src/shared/ai/Strategy.cpp
@@ -0,0 +1,21 @@
+/*
+ * File             Strategy.cpp
+ * Directory        ai
+ * Created Date     Sunday, January 16th 2022, 4:51:30 pm
+ * Author           Nouhou KANE
+ * Email            nouhou.kane@ensea.fr
+ * 
+ * Copyright (c) 2022  ENSEA(FR)
+ * __________________________________________________________________________
+ */
+
+#include "Strategy.h"
+
+
+namespace ai {
+    Strategy::Strategy (ai::DeepAI* g_ai){
+        //this->g_ai = std::shared_ptr<DeepAI>(g_ai);
+    }
+
+    Strategy::~Strategy(){}
+}
